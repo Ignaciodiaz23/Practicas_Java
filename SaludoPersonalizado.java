1ac3d84b457eac512e70dcd9ab939ab33ae69cd4
Ignacio Diaz <diazjuanig2026@gmail.com>
2026-09-19 13:49:14 -0300

    Fix greeting message formatting in SaludoPersonalizado

--- SaludoPersonalizado.java
+++ SaludoPersonalizado.java
@@ -1,10 +1,10 @@
 public class SaludoPersonalizado {
     public static void main(String[] args){
         if (args.length > 0){
-            System.out.println("Hola!"+ args [0] +"Bienvenido a la ingenieria en Java.");
+            System.out.println("Hola!"+ args [0] +"Bienvenido a la ingenieria en Java!");
 
         } else {
-            System.out.println("Hola, Desarrollador Anonimo! Pasa tu nombre como argumento.");
+            System.out.println("Hola, Desarrollador Anonimo! Pasa tu nombre como argumento:");
             
         }
     }
