3981ed1efb0791b2efabddbad081631e922d2230
Ignacio Diaz <diazjuanig2026@gmail.com>
2026-09-20 20:43:09 -0300

    Add documentation for Usuario class
    Added documentation comments to the Usuario class.

--- Practica/Usuario.java
+++ Practica/Usuario.java
@@ -1,12 +1,17 @@
+/**
+ * Representa a un usuario del sistema con sus datos de identificación.
+ * Aplica el principio de encapsulamiento protegiendo sus atributos.
+ */
 public class Usuario {
+    // Encapsulamiento: atributos privados accesibles solo a través de getters
     private String nombre;
     private int idUsuario;
-
+// Constructor: 'this' diferencia el atributo de instancia del parámetro recibido
     public Usuario(String nombre, int idUsuario) {
         this.nombre = nombre;
         this.idUsuario = idUsuario;
     }
-
+// Métodos de acceso (Getters): proveen lectura controlada de los atributos privado
     public String getNombre() {
         return nombre;
     }
